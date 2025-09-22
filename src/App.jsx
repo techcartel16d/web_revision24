82b7e3e7e49cfff52c230ebbbf299d8de6f2e070
Aman <amanbiban025@gmail.com>
2025-09-22 17:32:58 +0530

    gk test route setup setting screen code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // OLD CODE
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import LoginPage from './pages/authPage/LoginPage';
 import RegisterPage from './pages/authPage/RegisterPage';
@@ -76,50 +75,52 @@ import AttemptedTestPage from './pages/attempted/AttemptedTestPage';
 import FreequizesInstructions from './pages/freeQuiz/FreequizesInstructions';
 import FreequizeAttend from './pages/freeQuiz/FreequizeAttend';
 import GkCapage from './pages/freeTests/gk&currentAffers/GkCapage';
+import GkCaTestInstructions from './pages/freeTests/gk&currentAffers/GkCaTestInstructions';
+import GkCAtestPage from './pages/freeTests/gk&currentAffers/Gk&CAtestPage';
 
 function App() {
-  useEffect(() => {
-    const handleContextMenu = (e) => {
-      e.preventDefault();
-    };
+  // useEffect(() => {
+  //   const handleContextMenu = (e) => {
+  //     e.preventDefault();
+  //   };
 
-    const handleKeyDown = (e) => {
-      const key = e.key.toUpperCase();
+  //   const handleKeyDown = (e) => {
+  //     const key = e.key.toUpperCase();
 
-      // Disable Right Click
-      if (e.type === 'contextmenu') {
-        e.preventDefault();
-      }
+  //     // Disable Right Click
+  //     if (e.type === 'contextmenu') {
+  //       e.preventDefault();
+  //     }
 
-      // Disable F12
-      if (key === 'F12') {
-        e.preventDefault();
-      }
+  //     // Disable F12
+  //     if (key === 'F12') {
+  //       e.preventDefault();
+  //     }
 
-      // Disable Ctrl/Cmd + Shift + [I, J, C]
-      if ((e.ctrlKey || e.metaKey) && e.shiftKey && ['I', 'J', 'C'].includes(key)) {
-        e.preventDefault();
-      }
+  //     // Disable Ctrl/Cmd + Shift + [I, J, C]
+  //     if ((e.ctrlKey || e.metaKey) && e.shiftKey && ['I', 'J', 'C'].includes(key)) {
+  //       e.preventDefault();
+  //     }
 
-      // Disable Ctrl/Cmd + U (view source)
-      if ((e.ctrlKey || e.metaKey) && key === 'U') {
-        e.preventDefault();
-      }
+  //     // Disable Ctrl/Cmd + U (view source)
+  //     if ((e.ctrlKey || e.metaKey) && key === 'U') {
+  //       e.preventDefault();
+  //     }
 
-      // ✅ Mac-specific: Shift + Command + C (again handled above, but extra sure)
-      if (e.metaKey && e.shiftKey && key === 'C') {
-        e.preventDefault();
-      }
-    };
+  //     // ✅ Mac-specific: Shift + Command + C (again handled above, but extra sure)
+  //     if (e.metaKey && e.shiftKey && key === 'C') {
+  //       e.preventDefault();
+  //     }
+  //   };
 
-    document.addEventListener('contextmenu', handleContextMenu);
-    document.addEventListener('keydown', handleKeyDown);
+  //   document.addEventListener('contextmenu', handleContextMenu);
+  //   document.addEventListener('keydown', handleKeyDown);
 
-    return () => {
-      document.removeEventListener('contextmenu', handleContextMenu);
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
+  //   return () => {
+  //     document.removeEventListener('contextmenu', handleContextMenu);
+  //     document.removeEventListener('keydown', handleKeyDown);
+  //   };
+  // }, []);
 
 
   useEffect(() => {
@@ -231,8 +232,8 @@ function App() {
 
         {/* GK & CA */}
         <Route path="/gk&ca-page" element={<SideBarLayout><GkCapage /></SideBarLayout>} />
-        <Route path="/free-quizes-instruction" element={<SideBarLayout><FreequizesInstructions /></SideBarLayout>} />
-        <Route path="/free-quizes-attend" element={<ProtectedRoute><FreequizeAttend /></ProtectedRoute>} />
+        <Route path="/gk-ca-test-instruction" element={<SideBarLayout><GkCaTestInstructions /></SideBarLayout>} />
+        <Route path="/gk-test-attend" element={<ProtectedRoute><GkCAtestPage /></ProtectedRoute>} />
 
 
 
--- src/pages/freeTests/gk&currentAffers/GkCaTestInstructions.jsx
+++ src/pages/freeTests/gk&currentAffers/GkCaTestInstructions.jsx
@@ -1,47 +1,47 @@
-
-import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { getUserDataDecrypted } from '../../../helpers/userStorage';
-import Checkbox from '../../../components/Checkbox';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getUserDataDecrypted } from "../../../helpers/userStorage";
+import Checkbox from "../../../components/Checkbox";
+import { secureSet } from "../../../helpers/storeValues";
 
 const GkCaTestInstructions = () => {
   const nav = useNavigate();
   const { state } = useLocation();
-   const [isChecked, setIsChecked] = useState(false);
-  console.log(state)
+  const [isChecked, setIsChecked] = useState(false);
+  console.log(state);
   const [userInfo, setUserInfo] = useState(null);
-    const [lang, setLang] = useState('Choose a language')
+  const [lang, setLang] = useState("Choose a language");
   const getUserInfo = async () => {
     const user = await getUserDataDecrypted();
     // console.log(user)
     setUserInfo(user);
   };
-
+  const selectedLang = async (e) => {
+    setLang(e.target.value);
+    try {
+      await secureSet("language", e.target.value);
+    } catch (error) {
+      console.log("Error in store language", error);
+    }
+  };
   useEffect(() => {
     getUserInfo();
   }, []);
 
   const subjects = state?.testInfo || {};
-  const totalQuestions = subjects?.no_of_question || 0
-  const totalMarks = parseInt(subjects.marks_per_question || 0)
-  const negativeMark = state?.testInfo?.negative_marks || '0';
+  const totalQuestions = subjects?.no_of_question || 0;
+  const totalMarks = parseInt(subjects.marks_per_question || 0);
+  const negativeMark = state?.testInfo?.negative_marks || "0";
   const duration = state?.testInfo?.time || 60;
 
-    const selectedLang = async (e) => {
-            setLang(e.target.value)
-            try {
-                await secureSet('language', e.target.value)
-    
-            } catch (error) {
-                console.log("Error in store language", error)
-            }
-    
-        }
+
 
   return (
     <div className="px-6 py-4">
-      <div className='flex justify-between items-center gap-5 my-3'>
-        <h1 className="text-xl font-semibold">{state?.testInfo?.title || ''}</h1>
+      <div className="flex justify-between items-center gap-5 my-3">
+        <h1 className="text-xl font-semibold">
+          {state?.testInfo?.title || ""}
+        </h1>
 
         {/* <div className='flex items-center justify-center gap-3'>
           <p className="text-2xl font-bold text-blue-800">
@@ -51,7 +51,7 @@ const GkCaTestInstructions = () => {
       </div>
 
       <div className="text-sm w-full bg-blue-400 text-left text-white px-3 py-2 mb-6 font-bold rounded">
-        Candidate Name : {userInfo && userInfo?.name || 'N/A'}
+        Candidate Name : {(userInfo && userInfo?.name) || "N/A"}
       </div>
 
       <div className="text-[15px] text-gray-800">
@@ -82,97 +82,216 @@ const GkCaTestInstructions = () => {
 
         <ol className="list-decimal pl-5 space-y-4">
           <li>
-            <p className="font-bold mb-2">Exam Overview / परीक्षा का संक्षिप्त विवरण</p>
+            <p className="font-bold mb-2">
+              Exam Overview / परीक्षा का संक्षिप्त विवरण
+            </p>
             <ul className="list-disc pl-5 space-y-1">
-              <li>Duration: <span className="font-bold text-blue-700">{duration} minutes</span> / समयावधि: <span className="font-bold text-blue-700">{duration} मिनट</span></li>
-              <li>Total Questions: <span className="font-bold text-blue-700">{totalQuestions}</span> / कुल प्रश्न: <span className="font-bold text-blue-700">{totalQuestions}</span></li>
-              <li>Negative Marking: {negativeMark} marks deducted for each wrong answer. / ऋणात्मक अंकन: प्रत्येक गलत उत्तर पर <span className="font-bold">{negativeMark}</span> अंक काटे जाएंगे।</li>
-              <li>Number of Sections displayed at any time: <span className="font-bold text-blue-700">{subjects.length}</span> / किसी भी समय पर प्रदर्शित अनुभागों की संख्या: <span className="font-bold text-blue-700">{subjects.length}</span></li>
+              <li>
+                Duration:{" "}
+                <span className="font-bold text-blue-700">
+                  {duration} minutes
+                </span>{" "}
+                / समयावधि:{" "}
+                <span className="font-bold text-blue-700">{duration} मिनट</span>
+              </li>
+              <li>
+                Total Questions:{" "}
+                <span className="font-bold text-blue-700">
+                  {totalQuestions}
+                </span>{" "}
+                / कुल प्रश्न:{" "}
+                <span className="font-bold text-blue-700">
+                  {totalQuestions}
+                </span>
+              </li>
+              <li>
+                Negative Marking: {negativeMark} marks deducted for each wrong
+                answer. / ऋणात्मक अंकन: प्रत्येक गलत उत्तर पर{" "}
+                <span className="font-bold">{negativeMark}</span> अंक काटे
+                जाएंगे।
+              </li>
+              <li>
+                Number of Sections displayed at any time:{" "}
+                <span className="font-bold text-blue-700">
+                  {subjects.length}
+                </span>{" "}
+                / किसी भी समय पर प्रदर्शित अनुभागों की संख्या:{" "}
+                <span className="font-bold text-blue-700">
+                  {subjects.length}
+                </span>
+              </li>
             </ul>
           </li>
 
           <li>
-            <p className="font-bold mb-2">Timing & Submission / समय और उत्तर जमा करना</p>
+            <p className="font-bold mb-2">
+              Timing & Submission / समय और उत्तर जमा करना
+            </p>
             <ul className="list-disc pl-5 space-y-1">
-              <li>The timer displayed at the top-right corner is controlled by the server. Remaining time will be shown there.<br />ऊपरी दाएँ कोने में दिखाई देने वाला टाइमर सर्वर द्वारा नियंत्रित होता है। शेष समय वहीं प्रदर्शित होगा।</li>
-              <li>Once the time is over, the exam will automatically be submitted. You do not need to submit it manually.<br />समय समाप्त होने पर परीक्षा स्वतः जमा हो जाएगी। मैन्युअल रूप से सबमिट करने की आवश्यकता नहीं है।</li>
+              <li>
+                The timer displayed at the top-right corner is controlled by the
+                server. Remaining time will be shown there.
+                <br />
+                ऊपरी दाएँ कोने में दिखाई देने वाला टाइमर सर्वर द्वारा नियंत्रित
+                होता है। शेष समय वहीं प्रदर्शित होगा।
+              </li>
+              <li>
+                Once the time is over, the exam will automatically be submitted.
+                You do not need to submit it manually.
+                <br />
+                समय समाप्त होने पर परीक्षा स्वतः जमा हो जाएगी। मैन्युअल रूप से
+                सबमिट करने की आवश्यकता नहीं है।
+              </li>
             </ul>
           </li>
 
           <li>
             <p className="font-bold mb-2">Language / भाषा</p>
             <ul className="list-disc pl-5 space-y-1">
-              <li>Apart from English, questions and options for all other subjects will be displayed in both English and Hindi languages<br />अंग्रेजी के अलावा, अन्य सभी विषयों के प्रश्न और विकल्प अंग्रेजी और हिंदी दोनों भाषाओं में प्रदर्शित किए जाएंगे</li>
-              <li>If you choose English, questions will still appear only in English.<br />यदि आपने English चुना, तो प्रश्न केवल अंग्रेज़ी में ही दिखाई देंगे।</li>
-              <li>If you choose Hindi, questions will appear in both Hindi and English.<br />यदि आपने Hindi चुना, तो प्रश्न हिंदी और अंग्रेज़ी दोनों में प्रदर्शित होंगे।</li>
-              <li>You can change your language preference during the exam for sections where it is allowed.<br />जहाँ अनुमति हो, वहाँ परीक्षा के दौरान भाषा वरीयता बदली जा सकती है।</li>
+              <li>
+                Apart from English, questions and options for all other subjects
+                will be displayed in both English and Hindi languages
+                <br />
+                अंग्रेजी के अलावा, अन्य सभी विषयों के प्रश्न और विकल्प अंग्रेजी
+                और हिंदी दोनों भाषाओं में प्रदर्शित किए जाएंगे
+              </li>
+              <li>
+                If you choose English, questions will still appear only in
+                English.
+                <br />
+                यदि आपने English चुना, तो प्रश्न केवल अंग्रेज़ी में ही दिखाई
+                देंगे।
+              </li>
+              <li>
+                If you choose Hindi, questions will appear in both Hindi and
+                English.
+                <br />
+                यदि आपने Hindi चुना, तो प्रश्न हिंदी और अंग्रेज़ी दोनों में
+                प्रदर्शित होंगे।
+              </li>
+              <li>
+                You can change your language preference during the exam for
+                sections where it is allowed.
+                <br />
+                जहाँ अनुमति हो, वहाँ परीक्षा के दौरान भाषा वरीयता बदली जा सकती
+                है।
+              </li>
             </ul>
           </li>
 
           <li>
             <p className="font-bold mb-2">Navigation / नेविगेशन</p>
             <ul className="list-disc pl-5 space-y-1">
-              <li>All sections remain accessible throughout the exam. You can switch between sections or questions freely.<br />सभी अनुभाग परीक्षा के दौरान खुले रहते हैं। आप किसी भी अनुभाग या प्रश्न पर जा सकते हैं।</li>
-              <li>Use the "Previous" or "Save & Next" buttons to move between questions. Use "Mark for Review" to revisit questions.<br />"Previous" या "Save & Next" का उपयोग करें। किसी प्रश्न को बाद में देखने के लिए "Mark for Review" चुनें।</li>
-              <li>After the last question of a section, clicking "Save & Next" will take you to the next section.<br />अंतिम प्रश्न के बाद "Save & Next" पर क्लिक करने से अगला अनुभाग खुलेगा।</li>
+              <li>
+                All sections remain accessible throughout the exam. You can
+                switch between sections or questions freely.
+                <br />
+                सभी अनुभाग परीक्षा के दौरान खुले रहते हैं। आप किसी भी अनुभाग या
+                प्रश्न पर जा सकते हैं।
+              </li>
+              <li>
+                Use the "Previous" or "Save & Next" buttons to move between
+                questions. Use "Mark for Review" to revisit questions.
+                <br />
+                "Previous" या "Save & Next" का उपयोग करें। किसी प्रश्न को बाद
+                में देखने के लिए "Mark for Review" चुनें।
+              </li>
+              <li>
+                After the last question of a section, clicking "Save & Next"
+                will take you to the next section.
+                <br />
+                अंतिम प्रश्न के बाद "Save & Next" पर क्लिक करने से अगला अनुभाग
+                खुलेगा।
+              </li>
             </ul>
           </li>
 
           <li>
             <p className="font-bold mb-2">Answering / उत्तर देना</p>
             <ul className="list-disc pl-5 space-y-1">
-              <li>Each question contains four options; only one is correct.<br />हर प्रश्न के चार विकल्प होते हैं, केवल एक सही होता है।</li>
-              <li>The answer is saved only when you click "Save & Next".<br />"Save & Next" पर क्लिक करने के बाद ही उत्तर सुरक्षित होता है।</li>
-              <li>To modify a saved answer, revisit the question, make changes, and save it again.<br />उत्तर बदलने के लिए प्रश्न पर जाएँ और पुनः सुरक्षित करें।</li>
+              <li>
+                Each question contains four options; only one is correct.
+                <br />
+                हर प्रश्न के चार विकल्प होते हैं, केवल एक सही होता है।
+              </li>
+              <li>
+                The answer is saved only when you click "Save & Next".
+                <br />
+                "Save & Next" पर क्लिक करने के बाद ही उत्तर सुरक्षित होता है।
+              </li>
+              <li>
+                To modify a saved answer, revisit the question, make changes,
+                and save it again.
+                <br />
+                उत्तर बदलने के लिए प्रश्न पर जाएँ और पुनः सुरक्षित करें।
+              </li>
             </ul>
           </li>
 
           <li>
-            <p className="font-bold mb-2">Additional Notes / अतिरिक्त निर्देश</p>
+            <p className="font-bold mb-2">
+              Additional Notes / अतिरिक्त निर्देश
+            </p>
             <ul className="list-disc pl-5 space-y-1">
-              <li>The system saves each response automatically and submits it when time ends.<br />प्रत्येक उत्तर स्वतः सहेजा जाता है और समय समाप्त होने पर सबमिट हो जाता है।</li>
-              <li>If you have doubts, you can report the question.<br />यदि कोई संदेह हो तो प्रश्न की रिपोर्ट कर सकते हैं।</li>
-              <li>In case of technical issues, contact support.<br />तकनीकी समस्याओं के लिए सहायता टीम से संपर्क करें।</li>
+              <li>
+                The system saves each response automatically and submits it when
+                time ends.
+                <br />
+                प्रत्येक उत्तर स्वतः सहेजा जाता है और समय समाप्त होने पर सबमिट
+                हो जाता है।
+              </li>
+              <li>
+                If you have doubts, you can report the question.
+                <br />
+                यदि कोई संदेह हो तो प्रश्न की रिपोर्ट कर सकते हैं।
+              </li>
+              <li>
+                In case of technical issues, contact support.
+                <br />
+                तकनीकी समस्याओं के लिए सहायता टीम से संपर्क करें।
+              </li>
             </ul>
           </li>
         </ol>
 
+        <div className="py-3 flex flex-col gap-3">
+          <select
+            onChange={selectedLang}
+            className="border p-1 rounded-md bg-slate-200 border-slate-300 "
+          >
+            <option value="Choose a language">Choose a language</option>
+            <option value="en">English</option>
+            <option value="hi">Hindi</option>
+          </select>
 
-         <div className='py-3 flex flex-col gap-3'>
+          <Checkbox
+            id="example"
+            // links={[
+            //     {
+            //         link: '/terms-of-service',
+            //         link_name: 'Terms & Conditions'
+            //     },
+            //     {
+            //         link: '/privacy-policy',
+            //         link_name: 'Privacy Policy'
+            //     },
 
-                <select onChange={selectedLang} className='border p-1 rounded-md bg-slate-200 border-slate-300 '>
-                    <option value="Choose a language">Choose a language</option>
-                    <option value="en">English</option>
-                    <option value="hi">Hindi</option>
-                </select>
+            // ]}
+            label={`I have carefully read and fully understood all the instructions. I agree to follow them honestly and will not use any unfair means during this examination. I understand that attempting to gain an advantage for myself or others through dishonest practices will result in immediate disqualification.The decision of Revision24.com will be final in these matters and cannot be appealed.`}
+            checked={isChecked}
+            onChange={setIsChecked}
+          />
+        </div>
 
-                <Checkbox
-                    id="example"
-                    // links={[
-                    //     {
-                    //         link: '/terms-of-service',
-                    //         link_name: 'Terms & Conditions'
-                    //     },
-                    //     {
-                    //         link: '/privacy-policy',
-                    //         link_name: 'Privacy Policy'
-                    //     },
-
-                    // ]}
-                    label={`I have carefully read and fully understood all the instructions. I agree to follow them honestly and will not use any unfair means during this examination. I understand that attempting to gain an advantage for myself or others through dishonest practices will result in immediate disqualification.The decision of Revision24.com will be final in these matters and cannot be appealed.`}
-                    checked={isChecked}
-                    onChange={setIsChecked}
-                />
-            </div>
-        
-
-        <p className="text-center font-semibold text-green-700 mt-6">Good Luck! / शुभकामनाएँ!</p>
+        <p className="text-center font-semibold text-green-700 mt-6">
+          Good Luck! / शुभकामनाएँ!
+        </p>
       </div>
 
       <div className="flex justify-center my-6">
         <button
           className="bg-blue-700 text-white px-6 py-2 cursor-pointer rounded hover:bg-blue-800"
-          onClick={() => nav("/practice-test-attend-quiz", { state })}
+          onClick={() => nav("/gk-test-attend", { state })}
           // onClick={()=> console.log(userInfo)}
         >
           Agree & Get Start
